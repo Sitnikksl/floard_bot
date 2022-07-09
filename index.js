74fbb8383565fb14b40483478c358712de297d86
ychacarr <yanyablonski@gmail.com>
2022-07-09 17:22:19 +0700

    Подключил sqlite3

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import fetch from "node-fetch";
 import TelegramApi from "node-telegram-bot-api";
+import sqlite3 from "sqlite3"
+
+
 let nodePath = process.argv[2];
 const token = process.argv[2];
 const bot = new TelegramApi(token, {polling: true})
 
+let testFunc = (err, answer) =>{
+    if (answer != undefined) {
+        console.log(answer[0]['ID']);
+    }
+}
+
 const start = () =>{
+
+    const db = new sqlite3.Database("./FloardDatabase.db");
+    let test = db.all('SELECT * FROM Members;', testFunc);
 
     bot.setMyCommands([
         {command: '/start', description: 'Команда старта'}
@@ -15,7 +27,7 @@ const start = () =>{
         const chatId = msg.chat.id;
 
         if (text === '/start'){
-            return bot.sendMessage(chatId, 'Hello World');
+            return bot.sendMessage(chatId, 'Hello world!');
          }
 
     })
