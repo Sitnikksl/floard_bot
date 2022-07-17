ff4ae4468d2eb0bb689e6704088a1ad541908fc5
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-07-17 14:46:28 +0700

    затер старые клиентские функции

--- index.js
+++ index.js
@@ -5,67 +5,42 @@ import * as DB from "./classes.js"
 
 // #region TEST DB.Database
 
-//let db = new DB.Database('database.json');
+let db = new DB.Database('database.json');
 // console.log(db);
-// db.save();
+db.save();
 
-//#endregion
+// #endregion
 
-//#region main code
+// #region main code
 
-// let todayActiveMembers = dataBase.members;
-// let data = JSON.stringify(tempBase, null, '\t');
-// fs.writeFileSync('./dataBases/dataBase.json', data);
+let nodePath = process.argv[2];
+const token = process.argv[2];
+const bot = new TelegramApi(token, {polling: true})
 
-
-// let nodePath = process.argv[2];
-// const token = process.argv[2];
-// const bot = new TelegramApi(token, {polling: true})
-
-// const start = () =>{
+const start = () =>{
     
-//     bot.setMyCommands([
-//         {command: '/start', description: 'Команда старта'},
-//         {command: '/members', description: 'Узнать кто есть в наполках'},
-//         {command: '/firstmove', description: 'Узнать кто ходит первым'},
-//         {command: '/roll', description: 'Бросить кубик'}
-//     ])
+    bot.setMyCommands([
+        {command: '/start', description: 'Команда старта'},
+        {command: '/members', description: 'Узнать кто есть в наполках'},
+        {command: '/firstmove', description: 'Узнать кто ходит первым'},
+        {command: '/roll', description: 'Бросить кубик'}
+    ])
 
-//     bot.on('message', async msg=>{
-//         const text = msg.text;
-//         const chatId = msg.chat.id;
+    bot.on('message', async msg=>{
+        const text = msg.text;
+        const chatId = msg.chat.id;
 
-//         if (text === '/start'){
-//             await bot.sendMessage(chatId, "Добро пожаловать в наполочного бота!");
-//             // await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/b0d/85f/b0d85fbf-de1b-4aaf-836c-1cddaa16e002/1.gif');
-//         }else if(text ==='/firstmove'){
-//             let firstMoveMessage = 'Посылаю сигналы вселенной...' + '\n' + 'Иии... первым будет ходить...' + '\n' + '🥁🥁🥁' + '\n';
-//             await bot.sendMessage(chatId, firstMoveMessage);
-//             bot.sendAnimation(chatId, 'https://i.gifer.com/cE1.gif');
-//             function getRandomInt(max) {
-//                 return Math.floor(Math.random() * max);
-//             }
-//             let randomCount = getRandomInt(tempBase.members.length);
-//             let firstMan = tempBase.members[randomCount].Name 
-//             + ' ' + tempBase.members[randomCount].SurName
-//             + ' 🎉';
-//             setTimeout(() => {
-//                 bot.sendMessage(chatId, firstMan)
-//             }, 3000);
+        if (text === '/start'){
+            await bot.sendMessage(chatId, "Добро пожаловать в наполочного бота!");
+        }else if(text ==='/firstmove'){
             
-//         }else if(text === '/members'){
-//             let allMembers = 'Вот текущий состав наполок: ' + '\n' + '\n';
-            
-//             for(let i=0;i<tempBase.members.length; i++){
-//                 allMembers += '✨ ' + tempBase.members[i].SurName + ' ' + tempBase.members[i].Name + '\n'
-//             }
-//             bot.sendMessage(chatId, allMembers)
-//         }
-//         else if(text === '/roll'){
-//             bot.sendDice(chatId);
-//         }
-//     })
-// }
-// start();
+        }else if(text === '/members'){
+        }
+        else if(text === '/roll'){
+            bot.sendDice(chatId);
+        }
+    })
+}
+start();
 
-//#endregion
+// #endregion
